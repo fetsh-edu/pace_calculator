fcd21262ba43b2138da262b86dd3d2543b521d78
fetsh-edu <fetsh.me@gmail.com>
2021-04-04 23:51:36 +0300

    Add coordinator layout

--- app/src/main/java/me/fetsh/pacecalculator/MainActivity.java
+++ app/src/main/java/me/fetsh/pacecalculator/MainActivity.java
@@ -1,11 +1,11 @@
 package me.fetsh.pacecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mPaceInput = findViewById(R.id.pace_picker);
         mSpeedInput = findViewById(R.id.speed_picker);
